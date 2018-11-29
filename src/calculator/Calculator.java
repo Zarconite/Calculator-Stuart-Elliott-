5695ef1d9456cd387f949d6e46edfc24f55a8a3f
Zarconite <zarconite@gmail.com>
2018-11-29 10:21:37 +0000

    Added more tests/half implemented the four length array

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,17 +4,26 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		
-		inputNumbers(5,3,15);
+		input(2,4,8,16);
 
 	}
 	
-	public static String inputNumbers(int num1, int num2, int num3) {
+	public static String input(int num1, int num2, int num3) {
+		int[] array3= {num1,num2,num3};
+		return inputNumbers(array3);
+	}
+	
+	public static String input(int num1, int num2, int num3,int num4) {
+		int[] array4= {num1,num2,num3,num4};
+		return inputNumbers(array4);
+	}
+	
+	public static String inputNumbers(int[] array) {
 		
-		int[] array= {num1,num2,num3};
 		int length=array.length;
 		String Output = "";
 		
-		for(int i=0; i<(length); i++) {
+		for(int i=0; i<length; i++) {
 			
 			if(array[0]+array[1]==array[2]) 
 			{
@@ -49,10 +58,20 @@ public class Calculator {
 				Output=Output+(array[2]+"/"+array[1]+"="+array[0]+" ");
 			}
 			
+			if(array.length==3) {
 			int placeholder=array[2];
 			array[2]=array[1];
 			array[1]=array[0];
 			array[0]=placeholder;
+			}
+			
+			if(array.length==4) {
+			int placeholder=array[3];
+			array[3]=array[2];
+			array[2]=array[1];
+			array[1]=array[0];
+			array[0]=placeholder;
+			}
 			
 		}
 		
--- src/testCalculator/TestCalculator.java
+++ src/testCalculator/TestCalculator.java
@@ -7,27 +7,57 @@ import calculator.Calculator;
 public class TestCalculator {
 
 	@Test
-    public void Inputfivethreefifteen() {
-		assertEquals("Should return expected results","5*3=15 15/3=5 15/5=3 3*5=15 ", Calculator.inputNumbers(5,3,15));
+    public void InputFiveThreeFifteen() {
+		assertEquals("Should return expected results","5*3=15 15/3=5 15/5=3 3*5=15 ", Calculator.input(5,3,15));
 
     }
 	@Test
-    public void Inputtwothreefive() {
-		assertEquals("Should return expected results","2+3=5 5-3=2 5-2=3 3+2=5 ", Calculator.inputNumbers(2,3,5));
+    public void InputTwoThreeFive() {
+		assertEquals("Should return expected results","2+3=5 5-3=2 5-2=3 3+2=5 ", Calculator.input(2,3,5));
 
     }
-	/*
-	@Test
-    public void FactorialsTrue1() {
-		assertEquals("Should return ",15, Calculator.inputNumbers(5,3,15));
-
-    }
-	@Test
-    public void FactorialsTrue1() {
-		assertEquals("Should return ",15, Calculator.inputNumbers(5,3,15));
-
-    }*/
 	
+	@Test
+	public void InputFourTwoEight() {
+		assertEquals("Should return expected results","4*2=8 8/2=4 8/4=2 2*4=8 ", Calculator.input(4,2,8));
+
+	}	
+
+	@Test
+	public void InputSixTwoTwelve() {
+		assertEquals("Should return expected results","6*2=12 12/2=6 12/6=2 2*6=12 ", Calculator.input(6,2,12));
+
+    }
+
+	@Test
+	public void InputSixTwoThree() {
+		assertEquals("Should return expected results","6/2=3 3*2=6 2*3=6 6/3=2 ", Calculator.input(6,2,3));
+
+	}   
+	
+	@Test
+	public void InputNineTwelveOneHundredAndEight() {
+		assertEquals("Should return expected results","9*12=108 108/12=9 108/9=12 12*9=108 ", Calculator.input(9,12,108));
+
+	}	
+	
+	@Test
+	public void InputFourSixteenSixtyFour() {
+		assertEquals("Should return expected results","4*16=64 64/16=4 64/4=16 16*4=64 ", Calculator.input(4,16,64));
+
+	}	
+	
+	@Test
+	public void InputTwoFourSixThree() {
+		assertEquals("Should return expected results","2+4=6 6-4=2 6/3=2 2*3=6 ", Calculator.input(2,4,6,3));
+
+	}	
+	
+	@Test
+	public void InputEightFourThreeSix() {
+		assertEquals("Should return nothing","", Calculator.input(8,4,3,6));
+
+	}	
 }
 
 
